518158878e21ef035dc8d2ac4128e97efa0806c7
youssefawwad88 <youssefawwad88@gmail.com>
2025-07-31 14:02:31 -0400

    Update _Master_Screener_Hub.py

--- dashboard/Pages/_Master_Screener_Hub.py
+++ dashboard/Pages/_Master_Screener_Hub.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.set_page_config(page_title="Master Screener", layout="wide")
+st.title("📈 Master Screener Hub")
+st.write("This is where all signals from all screeners will be consolidated.")
