4f3500df649394be9422f031c1a0ec133019c8f5
youssefawwad88 <youssefawwad88@gmail.com>
2025-07-31 14:04:50 -0400

    Update _System_Settings.py

--- dashboard/Pages/_System_Settings.py
+++ dashboard/Pages/_System_Settings.py
@@ -1,1 +1,6 @@
 
+import streamlit as st
+
+st.set_page_config(page_title="System Settings", layout="wide")
+st.title("📈 System Settings")
+st.write("This is where all signals from all screeners will be consolidated.")
