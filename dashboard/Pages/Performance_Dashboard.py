e2fa324313112c963f68800849514b523157774f
youssefawwad88 <youssefawwad88@gmail.com>
2025-07-31 14:04:08 -0400

    Update Performance_Dashboard.py

--- dashboard/Pages/Performance_Dashboard.py
+++ dashboard/Pages/Performance_Dashboard.py
@@ -1,1 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Performance Dashboard", layout="wide")
+st.title("📈 Performance Dashboard")
+st.write("This is where all signals from all screeners will be consolidated.")
 
