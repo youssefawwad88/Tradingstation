63cb53d2feab7e3e4b957805e95e0d72920cdedc
youssefawwad88 <youssefawwad88@gmail.com>
2025-07-31 14:03:13 -0400

    Update _Strategy_Tabs.py

--- dashboard/Pages/_Strategy_Tabs.py
+++ dashboard/Pages/_Strategy_Tabs.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.set_page_config(page_title="Strategy Tabs", layout="wide")
+st.title("📈 Master Screener Hub")
+st.write("This is where all signals from all screeners will be consolidated.")
