b177f77186154750b1ef88d5086c44712668ceb4
youssefawwad88 <youssefawwad88@gmail.com>
2025-07-31 14:05:19 -0400

    Update _Trade_Journal.py

--- dashboard/Pages/_Trade_Journal.py
+++ dashboard/Pages/_Trade_Journal.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.set_page_config(page_title="Trade Journal", layout="wide")
+st.title("📈 Trade Journal")
+st.write("This is where all signals from all screeners will be consolidated.")
